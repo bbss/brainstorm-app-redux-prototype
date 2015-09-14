19ebf00c10e71f6b70f133bf9522d45f5558f1dc
bbss <baruchberger@gmail.com>
2015-09-14 16:02:06 +0800

    Redux store

--- src/App.js
+++ src/App.js
@@ -9,3 +9,20 @@ export default class App extends Component {
     )
   }
 }
+
+
+let anAction =  { type: "Something happened!", payload: "I said hello"}
+
+import { createStore } from 'redux';
+
+function exampleReducer (initialState = {}, action = {}) {
+  switch (action.type) {
+    case 'Something happened!':
+      console.log(action.payload)
+      return initialState.something = action.payload
+    default:
+      return initialState
+  }
+}
+let store = createStore(exampleReducer);
+store.dispatch(anAction)
