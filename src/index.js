a6b12b4241c6ce40b4eac45205490c78f7b34459
bbss <baruchberger@gmail.com>
2015-09-14 15:45:57 +0800

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import App from './App'
-import { graphReducer } from './App'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 
-let store = createStore(graphReducer)
-exports.store = store
-React.render(
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
+React.render(<App />,
   document.getElementById('root'))
 
