babe58d606b2199578d4a2c652c11c0a3ef21c3b
bbss <baruchberger@gmail.com>
2015-09-14 16:11:09 +0800

    React and redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component, findDOMNode } from 'react'
+import { store } from './index'
+import { connect } from 'react-redux'
 
+@connect((state) => ({
+  state: state
+}))
 export default class App extends Component {
   render() {
+    const { state } = this.props
     return (
       <div>
+        {state}
         Hello world!
       </div>
     )
@@ -13,9 +20,7 @@ export default class App extends Component {
 
 let anAction =  { type: "Something happened!", payload: "I said hello"}
 
-import { createStore } from 'redux';
-
-function exampleReducer (initialState = {}, action = {}) {
+export function exampleReducer (initialState = {}, action = {}) {
   switch (action.type) {
     case 'Something happened!':
       console.log(action.payload)
@@ -24,5 +29,3 @@ function exampleReducer (initialState = {}, action = {}) {
       return initialState
   }
 }
-let store = createStore(exampleReducer);
-store.dispatch(anAction)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import App from './App'
+import { exampleReducer } from './App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-React.render(<App />,
+let store = createStore(exampleReducer)
+exports.store = store
+React.render(
+  <Provider store={store}>
+    {() => <App />}
+  </Provider>,
   document.getElementById('root'))
-
